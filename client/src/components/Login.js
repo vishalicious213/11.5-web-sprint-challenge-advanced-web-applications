17bbd38b886ec0771668eb3eb14cc94df45a663d
Vish Singh <vishalicious213@gmail.com>
2020-07-17 16:58:23 -0700

    set up username & password state in Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
