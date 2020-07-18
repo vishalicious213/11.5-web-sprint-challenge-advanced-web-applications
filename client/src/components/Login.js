0e310eef117b37a1d409df3664c8d6bd76da6a9f
Vish Singh <vishalicious213@gmail.com>
2020-07-17 17:35:12 -0700

    saving token to localStorage. pushing /bubblepage to browser history

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [username, setUsername] = useState('');
@@ -21,7 +21,9 @@ const Login = () => {
     axios
       .post('http://localhost:5000/api/login', { username, password})
       .then(results => {
-        console.log('Results: ', results.data.payload);
+        // console.log('Results: ', results.data.payload);
+        localStorage.setItem('token', results.data.payload);
+        props.history.push('/bubblepage');
       })
       .catch(error => {
         console.log('Login error: ', error);
